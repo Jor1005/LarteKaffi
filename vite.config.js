3a32a2d6d016fca8cf90448dd2896fea470e76b9
Jor1005 <abanto1005@gmail.com>
2025-02-01 15:25:05 -0500

    vite confi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/LarteKaffi/", // Agrega el nombre del repositorio aquí
   plugins: [react()],
 })
