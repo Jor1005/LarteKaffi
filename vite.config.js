da60212a563b3abbe792a525043d22c5ffa24df9
Jor1005 <abanto1005@gmail.com>
2025-02-04 00:05:14 -0500

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/LarteKaffi/", // Agregar el nombre del repositorio
   plugins: [react()],
+  base: "/LarteKaffi/",  // 👈 Agrega esta línea con el nombre de tu repositorio
 })
