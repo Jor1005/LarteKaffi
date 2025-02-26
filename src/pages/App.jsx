79e53d1942c0402d160e31c45cfb726c1238b234
Jor1005 <abanto1005@gmail.com>
2025-02-26 02:53:35 -0500

    arreglo de estilos 1

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from '../componentes/nav';
 import Inicio from './inicio';
 import SobreNosotros from './sobrelarte';
-import Productos from './inicio/productos';
+import Productos from './inicio/categorias';
 import Contacto from '../componentes/contacto';
 import Footer from '../componentes/footer';
 import GlobalStyles from '../componentes/globalstyles';
---
+++ src/pages/inicio/categorias/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./productos.css"
+
+function Productos() {
+    return (
+      <section id="productos" className="productos">
+        <h1>CATEGORIAS</h1>
+        <div className="categorias">
+          <div className="producto" id="latteart" > <img src="img/latte.png" alt="Latte Art" />
+          <p>Latte Art</p> 
+          </div>
+          <div className="producto" id="balanza"><img src="img/latte.png" alt="Latte Art" /> Balanzas</div>
+          <div className="producto" id="expresso"><img src="img/latte.png" alt="Latte Art" />Expresso Bar</div>
+          <div className="producto" id="hervidores"><img src="img/latte.png" alt="Latte Art" />Hervidores inteligente</div>
+          <div className="producto" id="brewbar"> <img src="img/latte.png" alt="Latte Art" />Brew Bar</div>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Productos;
+  
