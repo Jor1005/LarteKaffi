9b1ddd3dc0bab0bf59098383583888560175e614
Jor1005 <abanto1005@gmail.com>
2025-07-29 23:42:54 -0500

    contacto whatsapp

--- src/componentes/contacto/index.jsx
+++ src/componentes/contacto/index.jsx
@@ -5,6 +5,12 @@ function contacto() {
   return (
     <section className="contacto">
       <h1>CONTACTO</h1>
+      <div className="contacto__container">
+        <a className="whatsapp" href="https://wa.me/51922165273?text=Hola%20estoy%20interesado%20en%20su%20producto" target="_blank" rel="noopener noreferrer">
+            <img src="\img\WhatsApp.svg.webp" alt="" />      
+        </a>
+      </div>
+      
     </section>
   );
 }
